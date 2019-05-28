213bdf6c7c11c8d1c37a1bcadf9a19ebe183c5d7
Iain Moncrief <Iainmoncrief@gmail.com>
2019-05-27 22:22:15 -1000

    created profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,10 +13,33 @@ class ProfileController extends Controller
      */
     public function index()
     {
+        // Returns the
         return view('profile', [
             'user' => Auth::user(),
         ]);
     }
 
+    /**
+     * Show the requested user profile.
+     * @param email
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function view(string $email)
+    {
+
+        // Tries to query the user based on the email, along with the profile object
+        $user = App\User::where('email', $email)->with('profile')->first();
+
+        // Checks if the user was found
+        if ($user) {
+            // Return the view if the user can be found.
+            return view('profile', [
+                'user' => $user,
+            ]);
+        } else {
+            // Returns a blank view and an error.
+            return view('profile');
+        }
+    }
 
 }
