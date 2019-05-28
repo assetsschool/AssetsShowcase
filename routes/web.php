f638c3b09670d45517139c25f0e810b4ac161b83
Iain Moncrief <Iainmoncrief@gmail.com>
2019-05-27 20:03:59 -1000

    verification works as well as email connections

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes(['verify' => true]);
+
+Route::get('/', 'HomeController@index');
+
+Route::get('/home', 'HomeController@index')->name('home');
